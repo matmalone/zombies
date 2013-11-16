aa1e64ca68bb8525b016533a0e4a9dbfd55fc2e8
Mat Malone <mmalone@datacare.com>
2013-11-15 19:31:34 -0800

    now use compass directions

--- zombie.rb
+++ zombie.rb
@@ -31,26 +31,27 @@ class Human < Entity
     def turn(map)
         super
 
-        heat_down = heat_up = heat_left = heat_right = 0
+        heat_south = heat_north = heat_west = heat_east = 0
 
         for y in 0..(map.width - 1)
             for x in 0..(map.height - 1)
                 if map.map[x][y].is_a? Zombie
                     dbg("#{@id}: found zombie at [#{x}][#{y}]")
                     if x < @x
-                        heat_left += 1
+                        heat_west += 1
                     elsif x > @x
-                        heat_right += 1
+                        heat_east += 1
                     end
                     if y < @y
-                        heat_down += 1
+                        heat_south += 1
                     elsif y > @y
-                        heat_up += 1
+                        heat_north += 1
                     end
                 end
             end
         end
-        dbg("heat_right: #{heat_right}, heat_left: #{heat_left}, heat_down: #{heat_down}, heat_up: #{heat_up}")
+        dbg("heat_east: #{heat_east}, heat_west: #{heat_west}, heat_south: #{heat_south}, heat_north: #{heat_north}")
+
 
     end    
 end
